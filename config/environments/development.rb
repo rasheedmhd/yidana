6d99481ca977ce1487bf906c36c73b05ca544ef1
Stefan Froelich <sfroelich01@gmail.com>
2023-10-28 02:41:02 +0000

    move rail initialization into separate file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,21 +3,6 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-    Bullet.alert         = true
-    # We want to raise because of hotwire. Else, we miss any alerts unless we manually reload the page.
-    Bullet.raise         = true
-    # All eager-loaded associations are intentional.
-    # Due to how our policy scopes are written, sometimes, they will not be used.
-    # It is less dangerous to have unused eager loading than if they are required and absent.
-    Bullet.unused_eager_loading_enable = false
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
